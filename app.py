538dfd744f07f6e5d207a972709627a4f369bb0a
Felipe Augusto Graciano <felipe98ju@hotmail.com>
2022-09-08 20:01:33 -0300

    Criação de funcs.py e 2 funções
    Criação de arquivo onde ficarão as funções do projeto, criação de função para fazer Select e Insert no banco de dados
    
    Co-Authored-By: AugustoBuin <111705690+AugustoBuin@users.noreply.github.com>

--- app.py
+++ app.py
@@ -2,18 +2,18 @@
 from importlib.metadata import requires
 from flask import Flask, render_template,request, url_for, redirect, session
 from flask_mysqldb import MySQL
+import funcs
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 # Conexão ao banco de dados
 app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_PORT'] = 3307 #Caso a porta seja a padrão, comentar linha.
+app.config['MYSQL_PORT'] = 3307 #Caso a porta seja a padrão, comentar linha.
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'mcs2809'
+app.config['MYSQL_PASSWORD'] = 'yyyetygvg1'
 app.config['MYSQL_DB'] = 'pynk'
 
 mysql = MySQL(app)
-
 # Bloco de Paginas.
 
 # Resumo dos comandos 
@@ -26,52 +26,37 @@ def index():
     return render_template('login.html')
 #------------------------------
 
-#Pagina de cadastro
-@app.route("/cadastro.html")
-def chama_cadastro():
-    return render_template('cadastro.html')
-#------------------------------
+@app.route("/home", methods = ['POST', 'GET'])
+def home():
+    return render_template('home.html')
 
 #Pagina de login
 @app.route("/cadastro.html", methods = ['POST', 'GET'])
 def cadastro():
     if request.method == "POST":
         #Variaveis vindas do FORM vindas do cadastro.html
-        name = request.form['name']
-        cpf = request.form['cpf']
-        endereco = request.form['endereco']
-        datanasc = request.form['datanasc']
-        genero = request.form['genero']
-        senha = request.form['senha']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO tb_usuario (cpf, nome, genero, endereco, senha, datanascimento) VALUES(%s,%s,%s,%s,%s,%s)''',(cpf,name,genero,endereco,senha,datanasc))
-        mysql.connection.commit()
-        cursor.close()
+        nome            = request.form['name']
+        cpf             = request.form['cpf']
+        endereco        = request.form['endereco']
+        datanascimento  = request.form['datanasc']
+        genero          = request.form['genero']
+        senha           = request.form['senha']
+        funcs.InsMySQL('tb_usuario',CampoBd=['cpf', 'nome', 'genero', 'endereco', 'senha', 'datanascimento'],CampoFm=[cpf,nome,genero,endereco,senha,datanascimento])
     return render_template('cadastro.html')
 #------------------------------
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
     if request.method == "POST":
-        cpf = request.form['cpf']
-        senha = request.form['senha']
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' SELECT * FROM tb_usuario WHERE cpf = %s AND senha = %s ''',(cpf,senha))
-        resultado = cursor.fetchone()
-        mysql.connection.commit()
-        cursor.close()
+        cpf         = request.form['cpf']
+        senha       = request.form['senha']
+        resultado   = funcs.SlcMySQL('tb_usuario',CampoBd=['cpf','senha'],CampoFm=[cpf,senha])
     if resultado:
-        session['login'] = True
-        session['nome'] = resultado[1]
-
-        return redirect(url_for('home'))
+        session['login']    = True
+        session['nome']     = resultado[1]
+        return home()
     else:
-        return render_template('login.html')
-
-
-@app.route("/home", methods = ['POST', 'GET'])
-def home():
-    return render_template('home.html')
+        return index()
 
 #Bloco para subir o site.
 if __name__ == "__main__":
---
+++ funcs.py
@@ -0,0 +1,43 @@
+from importlib.metadata import requires
+from flask import Flask, render_template,request, url_for, redirect, session
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+mysql = MySQL(app)
+
+def SlcMySQL(TabelaBd,CampoBd,CampoFm):
+    x=0
+    cursor = mysql.connection.cursor()
+    textoSQL = f' SELECT * FROM {TabelaBd} WHERE '
+    for values in CampoBd:
+        if x==0:
+            textoSQL+= f'{CampoBd[x]} = {CampoFm[x]} '
+        else:
+            textoSQL+= f' and {CampoBd[x]} = {CampoFm[x]}'
+        x+=1
+    cursor.execute(textoSQL)
+    resultado = cursor.fetchone()
+    mysql.connection.commit()
+    cursor.close()
+    return resultado
+
+def InsMySQL(TabelaBd,CampoBd,CampoFm):
+    x=0
+    ValuesBD = '('
+    ValuesFm = '('
+    cursor = mysql.connection.cursor()
+    textoSQL = f' INSERT INTO {TabelaBd} '
+    for values in CampoBd:
+        if values == CampoBd[-1]:
+            ValuesBD += f'{CampoBd[x]})'
+            ValuesFm += f'{CampoFm[x]})'
+        else:
+            ValuesBD += f'{CampoBd[x]},'
+            ValuesFm += f'{CampoFm[x]},'  
+        x+=1
+    textoSQL += f' {ValuesBD} VALUES{ValuesFm}' 
+    cursor.execute(textoSQL)
+    mysql.connection.commit()
+    cursor.close()
