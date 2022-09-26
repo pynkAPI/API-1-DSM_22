6ee435051b370bb03156871ab5506aa659cf425b
otavioabreu27 <otavio.abreu96@gmail.com>
2022-09-26 20:45:54 -0300

    Começo do desenvolvimento da função de insert na tabela transação

--- src/funcs.py
+++ src/funcs.py
@@ -1,6 +1,7 @@
 from importlib.metadata import requires
 from flask import Flask, render_template,request, url_for, redirect, session
 from flask_mysqldb import MySQL
+import datetime
 import random
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ def SlcMySQL(TabelaBd,CampoBd,CampoFm):
         else:
             textoSQL+= f' and {CampoBd[x]} = "{CampoFm[x]}"'
         x+=1
-    
+
     cursor.execute(textoSQL)
     resultado = cursor.fetchone()
     mysql.connection.commit()
@@ -35,7 +36,7 @@ def SlcEspecificoMySQL(TabelaBd,CampoBd,CampoFm, CampoEs):
             textoSQL += f'SELECT {CampoEs[y]}'
         else:
             textoSQL += f', {CampoEs[y]}'
-        y+=1 
+        y+=1
     textoSQL += f' FROM {TabelaBd}'
     for values in CampoBd:
         if x==0:
@@ -61,13 +62,13 @@ def InsMySQL(TabelaBd,CampoBd,CampoFm):
             ValuesFm += f'"{CampoFm[x]}")'
         else:
             ValuesBD += f'{CampoBd[x]},'
-            ValuesFm += f'"{CampoFm[x]}",'  
+            ValuesFm += f'"{CampoFm[x]}",'
         x+=1
-    textoSQL += f' {ValuesBD} VALUES{ValuesFm}' 
+    textoSQL += f' {ValuesBD} VALUES{ValuesFm}'
     cursor.execute(textoSQL)
     mysql.connection.commit()
     cursor.close()
-    
+
 def upMySQL(TabelaBd,CampoBd,CampoFm,CampoWr,CampoPs):
     x=0
     y=0
@@ -105,21 +106,21 @@ def DelMySQL(TabelaBd,CampoBd,CampoFm):
     resultado = cursor.fetchone()
     mysql.connection.commit()
     cursor.close()
-    return resultado    
+    return resultado
 
 
 def geraId(nome, agencia, cpf):
     #O id é gerado a partir de alguns processos de criptografia
     #os primeiros 4 digitos são gerados a partir do id da agência
-    #os próximos 4 digitos são valores randomicos do cpf do usuario 
-    #os próximos 4 digitos são valores criptografados a partir do nome do usuario 
+    #os próximos 4 digitos são valores randomicos do cpf do usuario
+    #os próximos 4 digitos são valores criptografados a partir do nome do usuario
     alfabetoCript = ['e', 'd', 'n', 'z', 'w', 'f', 't', 'u', 'p', 'o', 's', 'v', 'y', 'r', 'j', 'x', 'i', 'a', 'm', 'b', 'q', 'c', 'g', 'h', 'k', 'l']
     nome = nome.lower()
     arr_nome = nome.split(" ")
     letra_prim_nome = arr_nome[0][0:1]
     letra_seg_nome = ''
     if len(arr_nome)<1:
-        letra_seg_nome = arr_nome[1][0:1]   
+        letra_seg_nome = arr_nome[1][0:1]
     format_agencia = ""
     arr_cpf = []
     cont_caractere = 0
@@ -132,7 +133,7 @@ def geraId(nome, agencia, cpf):
             qt_zeros -= 1
         format_agencia += agencia
         agencia = format_agencia
-    
+
     nummeroConta = agencia
 
     #gerando um array com os valores splitados
@@ -145,7 +146,7 @@ def geraId(nome, agencia, cpf):
         random_index = random.randint(0, len(arr_cpf)-1)
         nummeroConta += arr_cpf [random_index]
         cont_caractere += 1
-    
+
     nummeroConta += str(alfabetoCript.index(letra_prim_nome))
     if letra_seg_nome:
         nummeroConta += str(alfabetoCript.index(letra_seg_nome))
@@ -153,10 +154,28 @@ def geraId(nome, agencia, cpf):
     #caso não some 12 caracteres esse while concatena zeros ao final da string
     while len(nummeroConta)<12:
         nummeroConta += '0'
-    
+
     return nummeroConta
 
 def TirarPontoeTraco(CPF):
     CPF = CPF.replace(".","")
     CPF = CPF.replace("-","")
     return CPF
+
+def InsTransacao(conta_origem, conta_destino, tipo, valor):
+    data = datetime.datetime.now()
+    cursor = mysql.connection.cursor()
+    textoSQL = ' INSERT INTO tb_transacao (id_conta_origem, id_conta_destino, Datatime, tipo, valor)'
+    textoSQL += f"VALUES ('{conta_origem}', '{conta_destino}', '{data}', '{tipo}', '{valor}');"
+    for values in CampoBd:
+        if values == CampoBd[-1]:
+            ValuesBD += f'{CampoBd[x]})'
+            ValuesFm += f'"{CampoFm[x]}")'
+        else:
+            ValuesBD += f'{CampoBd[x]},'
+            ValuesFm += f'"{CampoFm[x]}",'
+        x+=1
+    textoSQL += f' {ValuesBD} VALUES{ValuesFm}'
+    cursor.execute(textoSQL)
+    mysql.connection.commit()
+    cursor.close()
