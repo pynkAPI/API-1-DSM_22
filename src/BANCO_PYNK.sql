cac740a6a6556462983c0e36b0b2c141049e8d7e
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2022-11-06 22:48:37 -0300

    Ajuste no banco de dados, e na rota do html em "criar agência"

--- src/BANCO_PYNK.sql
+++ src/BANCO_PYNK.sql
@@ -20,7 +20,7 @@ CREATE TABLE tb_funcionario (
 id_funcionario int AUTO_INCREMENT PRIMARY KEY,
 id_usuario int NOT NULL,
 papel varchar(255) NOT NULL,
-num_matricula varchar(255) NOT NULL,
+num_matricola varchar(255) NOT NULL,
 login VARCHAR(255) NOT NULL, 
 FOREIGN KEY(id_usuario) REFERENCES tb_usuario (id_usuario)
 );
@@ -51,7 +51,8 @@ id_conta_destino int NOT NULL,
 Datatime datetime NOT NULL,
 tipo varchar(50) NOT NULL,
 valor double NOT NULL,
-status_transacao varchar(1) NOT NULL
+status_transacao varchar(1) NOT NULL,
+data_aceite_recusa DATETIME
 );
 
 CREATE TABLE tb_agencia (
@@ -67,7 +68,6 @@ CREATE TABLE tb_requisicoes(
 id_requisicao int AUTO_INCREMENT PRIMARY KEY,
 status_alteracao varchar(1) NOT NULL,
 id_usuario INT NOT NULL,
-id_funcionario INT,
 FOREIGN KEY(id_usuario) REFERENCES tb_usuario (id_usuario),
 descricao VARCHAR(255)
 );
@@ -98,50 +98,3 @@ INSERT INTO tb_regra_operacoes(descricao, porcentagem, valor_fixo, frequencia) V
 
 INSERT INTO tb_capitaltotal 
 VALUES(1, 10000,0);
-
-INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
-VALUES('GERENTE AGÊNCIA', '2',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
-
-INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
-VALUES('GERENTE AGÊNCIA 2', '3',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
-
-INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
-VALUES('GERENTE GERAL', '1',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
-
-INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
-VALUES('Miguel Carvalho', '23207568092',  'O', 'TESTE1', curdate(), 'teste', '1', 'doxito007@gmail.com');
-
-INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
-VALUES('Felipe Augusto Graciano', '91074953070',  'M', 'TESTE2', '2003-09-08', '123', '1', 'felipe98ju@hotmail.com');
-
-INSERT INTO tb_contabancaria(id_usuario, id_agencia, tipo, numeroconta, data_abertura, saldo, status_contabancaria)
-VALUES(4, 1, 'CONTA CORRENTE', 1234, curdate(), -200, '1');
-
-INSERT INTO tb_contabancaria(id_usuario, id_agencia, tipo, numeroconta, data_abertura, saldo, status_contabancaria)
-VALUES(5, 1, 'CONTA CORRENTE', 4321, curdate(), 0, '1');
-
-INSERT INTO tb_funcionario(papel, num_matricula, id_usuario, login) 
-VALUES('GERENTE DE AGÊNCIA', '0', 1, 'GA1');
-
-INSERT INTO tb_funcionario(papel, num_matricula, id_usuario, login) 
-VALUES('GERENTE DE AGÊNCIA', '0', 2, 'GA2');
-
-INSERT INTO tb_funcionario(papel, num_matricula, id_usuario, login) 
-VALUES('GERENTE GERAL', '0', 3, 'GG');
-
-INSERT INTO tb_agencia(localidade, id_funcionario, numero_agencia, status_agencia)
-VALUES('SP',2, '0001', 1);
-
-INSERT INTO tb_agencia(localidade, id_funcionario, numero_agencia, status_agencia)
-VALUES('RJ',1,'0002', 1);
-
-INSERT INTO tb_requisicoes(status_alteracao, id_usuario, descricao)
-VALUES ('0',1,'desejo alterar meu cpf para ...');
-
-INSERT INTO tb_cheque_especial(id_conta, data_inicio, data_atualizacao,data_final, valor_devido, ativo) VALUES(1, '2022-10-01','2022-10-01', NULL, -200, 1);
-
-select * from tb_requisicoes tr;
-select * from tb_usuario tu;
-select * from tb_agencia ta;
-select * from tb_contabancaria tc ;
-select * from tb_funcionario tf ;
