14edabbe5e8f0139f0908bae92b3387fc91a944e
Miguel.Carvalho <doxito007@gmail.com>
2022-11-09 20:30:49 -0300

    Atualização do Banco de dados CORRETO.

--- src/BANCO_PYNK.sql
+++ src/BANCO_PYNK.sql
@@ -49,10 +49,10 @@ id_transacao int PRIMARY KEY auto_increment,
 id_conta_origem int NOT NULL,
 id_conta_destino int NOT NULL,
 Datatime datetime NOT NULL,
+data_aceite_recusa datetime,
 tipo varchar(50) NOT NULL,
 valor double NOT NULL,
-status_transacao varchar(1) NOT NULL,
-data_aceite_recusa DATETIME
+status_transacao varchar(1) NOT NULL
 );
 
 CREATE TABLE tb_agencia (
@@ -98,3 +98,44 @@ INSERT INTO tb_regra_operacoes(descricao, porcentagem, valor_fixo, frequencia) V
 
 INSERT INTO tb_capitaltotal 
 VALUES(1, 10000,0);
+
+INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
+VALUES('GERENTE AGÊNCIA', '2',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
+
+INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
+VALUES('GERENTE AGÊNCIA 2', '3',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
+
+INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
+VALUES('GERENTE GERAL', '1',  'O', 'ENDERECO DOS BOBOS', curdate(), 'senha', '1', 'teste@gmail.com');
+
+INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
+VALUES('Miguel Carvalho', '23207568092',  'O', 'TESTE1', curdate(), 'teste', '1', 'doxito007@gmail.com');
+
+INSERT INTO tb_usuario (nome, cpf, genero, endereco, datanascimento, senha, ativo, email) 
+VALUES('Felipe Augusto Graciano', '91074953070',  'M', 'TESTE2', '2003-09-08', '123', '1', 'felipe98ju@hotmail.com');
+
+INSERT INTO tb_contabancaria(id_usuario, id_agencia, tipo, numeroconta, data_abertura, saldo, status_contabancaria)
+VALUES(4, 1, 'CONTA CORRENTE', 1234, curdate(), -200, '1');
+
+INSERT INTO tb_contabancaria(id_usuario, id_agencia, tipo, numeroconta, data_abertura, saldo, status_contabancaria)
+VALUES(5, 1, 'CONTA CORRENTE', 4321, curdate(), 0, '1');
+
+INSERT INTO tb_funcionario(papel, num_matricola, id_usuario, login) 
+VALUES('GERENTE DE AGÊNCIA', '0', 1, 'GA1');
+
+INSERT INTO tb_funcionario(papel, num_matricola, id_usuario, login) 
+VALUES('GERENTE DE AGÊNCIA', '0', 2, 'GA2');
+
+INSERT INTO tb_funcionario(papel, num_matricola, id_usuario, login) 
+VALUES('GERENTE GERAL', '0', 3, 'GG');
+
+INSERT INTO tb_agencia(localidade, id_funcionario, numero_agencia, status_agencia)
+VALUES('SP',2, '0001', 1);
+
+INSERT INTO tb_agencia(localidade, id_funcionario, numero_agencia, status_agencia)
+VALUES('RJ',1,'0002', 1);
+
+INSERT INTO tb_requisicoes(status_alteracao, id_usuario, descricao)
+VALUES ('0',1,'desejo alterar meu cpf para ...');
+
+INSERT INTO tb_cheque_especial(id_conta, data_inicio, data_atualizacao,data_final, valor_devido, ativo) VALUES(1, '2022-10-01','2022-10-01', NULL, -200, 1);
